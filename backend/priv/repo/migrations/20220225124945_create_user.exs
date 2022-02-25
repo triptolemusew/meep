cad0376f0d1741daf983d23bfeebcea235bdc9b0
faris <deneuve.crepes@gmail.com>
2022-02-25 22:31:51 +0800

    Add formatting for migration and seeds

--- backend/priv/repo/migrations/20220225124945_create_user.exs
+++ backend/priv/repo/migrations/20220225124945_create_user.exs
@@ -3,9 +3,9 @@ defmodule Backend.Repo.Migrations.CreateUser do
 
   def change do
     create table(:user) do
-      add :first_name, :string
-      add :last_name, :string
-      add :age, :integer
+      add(:first_name, :string)
+      add(:last_name, :string)
+      add(:age, :integer)
     end
   end
 end
--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -5,13 +5,11 @@ defmodule Backend.DBSeeders do
   alias Backend.Repo
 
   def insert_user do
-    Repo.insert!(
-      %User{
-        first_name: Faker.Person.first_name(),
-        last_name: Faker.Person.last_name(),
-        age: Faker.random_between(20, 30),
-      }
-    )
+    Repo.insert!(%User{
+      first_name: Faker.Person.first_name(),
+      last_name: Faker.Person.last_name(),
+      age: Faker.random_between(20, 30)
+    })
   end
 end
 
