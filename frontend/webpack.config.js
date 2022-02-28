0d304aa9ce5a1ff94e34c6a426c6d72dbe30292a
faris <deneuve.crepes@gmail.com>
2022-02-28 16:31:59 +0800

    Webpack with esbuild-loader

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,17 +1,18 @@
-const path = require("path");
+const path = require('path');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./main.js",
+  mode: 'development',
+  entry: './main.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].[contenthash].js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/',
   },
   devServer: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 8080,
     open: true,
     hot: true,
@@ -22,21 +23,21 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: [/webpack.config.js/, /build/, /assets/],
-        loader: "esbuild-loader",
+        loader: 'esbuild-loader',
         options: {
-          loader: "jsx",
-          target: "es2015",
+          loader: 'jsx',
+          target: 'es2015',
         },
       },
       {
         test: /\.css$/i,
         use: [
-          "style-loader",
-          "css-loader",
+          'style-loader',
+          'css-loader',
           {
-            loader: "esbuild-loader",
+            loader: 'esbuild-loader',
             options: {
-              loader: "css",
+              loader: 'css',
               minify: true,
             },
           },
@@ -44,19 +45,19 @@ module.exports = {
       },
       {
         test: /\.(jpg|png)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.svg$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Meep",
-      filename: "index.html",
-      template: path.resolve(__dirname, "main.ejs"),
+      title: 'Meep',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'main.ejs'),
     }),
   ],
 };
